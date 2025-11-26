09a916804f6d0a9a0457ecc770d02fb234437825
sreesha variseri <svariseri@ufl.edu>
2025-11-26 17:52:20 -0500

    added space in the grid for the button, which are mostly functional and added more options for the main menu

--- board.py
+++ board.py
@@ -13,39 +13,56 @@ class Board:
     #in total: 9x9 grid
     def draw(self):
         #fills screen color
-        self.screen.fill((255, 255, 255))
+        Board.screen.fill((255, 255, 255))
         #draw - line
         for i in range(4):
-            pygame.draw.line(Board.screen, (0, 0, 0), (0, i * 200), (self.height, i * 200), 15)
+            pygame.draw.line(Board.screen, (0, 0, 0), (0, i * 175), (self.height, i * 175), 15)
             #draw smaller lines
         for l in range(10):
-            pygame.draw.line(Board.screen, (0, 0, 0), (0, l * 200//3), (self.width, l * 200//3), 2)
+            pygame.draw.line(Board.screen, (0, 0, 0), (0, l * 175//3), (self.width, l * 175//3), 2)
 
         # draw | lines
         for j in range(4):
-            pygame.draw.line(Board.screen, (0, 0, 0), (j * 200, 0), (j * 200, self.width), 15)
+            pygame.draw.line(Board.screen, (0, 0, 0), (j * 175, 0), (j * 175, self.width), 15)
             #draw smaller lines
         for k in range(10):
-            pygame.draw.line(Board.screen, (0, 0, 0), (k * 200//3, 0), (k * 200//3, self.height), 2)
+            pygame.draw.line(Board.screen, (0, 0, 0), (k * 175//3, 0), (k * 175//3, self.height), 2)
+        #for cover up
+        Board.screen.fill((0,0,0),(0,520,self.width,self.height))
+        Board.screen.fill((0, 0, 0), (520, 0, self.width, self.height))
 
-    def select(self,row,col):
-        #marks current cell
-    def click(self,x,y):
-        #return x,y, else returns None
-    def clear(self):
-        #removed input from the user
-    def sketch(self,value):
-        #sets skecth value
-    def place_number(self,value):
-        #sets seketch vale to user entered function
-    def reset_to_original(self):
-        # resets all cells
-    def is_full(self):
-        #bla
-    def find_empty(self):
-        #s
-    def check_board(self):
-        #aa
+        #reset/restart/exit buttons
+        exit = pygame.image.load("exit.png")
+        exit_rect = exit.get_rect(center=(560, 150))
+        Board.screen.blit(exit, exit_rect)
+
+        rs = pygame.image.load("restart.png")
+        rs_rect = rs.get_rect(center=(560, 300))
+        Board.screen.blit(rs, rs_rect)
+
+        r = pygame.image.load("reset.png")
+        r_rect = r.get_rect(center=(560, 450))
+        Board.screen.blit(r, r_rect)
+
+
+    # def select(self,row,col):
+    #     #marks current cell
+    # def click(self,x,y):
+    #     #return x,y, else returns None
+    # def clear(self):
+    #     #removed input from the user
+    # def sketch(self,value):
+    #     #sets skecth value
+    # def place_number(self,value):
+    #     #sets seketch vale to user entered function
+    # def reset_to_original(self):
+    #     # resets all cells
+    # def is_full(self):
+    #     #bla
+    # def find_empty(self):
+    #     #s
+    # def check_board(self):
+    #     #aa
 
 class Cell:
     def __init__(self,value,row,col,screen):
@@ -102,7 +119,10 @@ def start_screen(screen):
     pygame.display.flip()
     while True:
         for event in pygame.event.get():
-            if event.type==pygame.MOUSEBUTTONDOWN:
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type==pygame.MOUSEBUTTONDOWN:
                 x,y=pygame.mouse.get_pos()
                 #to make it easier to understand
                 x=x//10
@@ -132,5 +152,28 @@ while start==False:
         if event.type==pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type==pygame.MOUSEBUTTONDOWN:
+                x,y=pygame.mouse.get_pos()
+                x = x // 10
+                y = y // 100
+                print(x,y)
+                if 53<=x<=58 and y==1: #exit button is pressed
+                    pygame.quit()
+                    sys.exit()
+                elif 53<=x<=58 and 2<=y<=3: #restart
+                    d=0
+                    c = Board(9 * 600, 9 * 600, d)
+                    while True:
+                        d = start_screen(Board.screen)
+                        if d != 0:
+                            c.difficulty = d
+                            c.draw()
+                            break
+                # elif 53 <= x <= 58 and y==4: #reset
+                #     #reset/clear all user inputted values
+                #elif input values
+        #3rd event is the movement system
+
+
     # a version of c.draw with numbers needed so values gets updated/displayed
     pygame.display.update()
